038cb36625e43c6137de527a97c57d209b07bb9b
pavel-035 <kdpavel8@gmail.com>
2021-11-30 04:58:12 +0500

    animate

--- dist/src/scripts/main.js
+++ dist/src/scripts/main.js
@@ -6,7 +6,6 @@ window.addEventListener('mousemove', function (e) {
     let windowWidth = window.innerWidth / 2;
     let windowHeight = window.innerHeight / 2;
 
-    console.log(windowWidth);
     if(windowWidth >= 960) {
         translateBlock.forEach(function(item) {
             let translateSpeed = item.getAttribute('data-animateSpeed')
@@ -15,95 +14,34 @@ window.addEventListener('mousemove', function (e) {
     }
 })
 
-// const sections = document.querySelectorAll('.cubes-fonts__color')
-// const animateClass = 'cubes-fonts__animate'
-// const options = {
-//     rootMargin: '0px',
-//     threshold: 0.25
-// }
-//
-// const callback = entries => {
-//     entries.forEach(entry => {
-//         let target = entry.target
-//         if (entry.intersectionRatio >= 0.5) {
-//             target.classList.add(animateClass);
-//         } else {
-//             target.classList.remove(animateClass);
-//         }
-//     })
-// }
-//
-// let observer = new IntersectionObserver(callback, options)
-//
-// sections.forEach((section, index) => {
-//     observer.observe(section)
-// })
 
-let isScrolling = false;
-
-window.addEventListener("scroll", throttleScroll, false);
-
-function throttleScroll(e) {
-    if (isScrolling === false) {
-        window.requestAnimationFrame(function() {
-            scrolling(e);
-            isScrolling = false;
-        });
-    }
-    isScrolling = true;
+// colors animate
+const section = document.getElementById('fonts')
+const colors = document.querySelectorAll('.cubes-fonts__color')
+const animateClass = 'cubes-fonts__animate'
+const options = {
+    rootMargin: '200px',
+    threshold: [0.0, 0.79, 0.8]
 }
 
-document.addEventListener("DOMContentLoaded", scrolling, false);
-
-let listItems = document.querySelectorAll("#mainContent ol li");
-let firstBox = document.querySelector("#firstBox");
-let secondBox = document.querySelector("#secondBox");
-
-function scrolling(e) {
-
-    if (isPartiallyVisible(firstBox)) {
-        firstBox.classList.add("active");
-
-        document.body.classList.add("colorOne");
-        document.body.classList.remove("colorTwo");
-    } else {
-        document.body.classList.remove("colorOne");
-        document.body.classList.remove("colorTwo");
-    }
-
-    if (isFullyVisible(secondBox)) {
-        secondBox.classList.add("active");
-
-        document.body.classList.add("colorTwo");
-        document.body.classList.remove("colorOne");
-    }
-
-    for (let i = 0; i < listItems.length; i++) {
-        let listItem = listItems[i];
-
-        if (isPartiallyVisible(listItem)) {
-            listItem.classList.add("active");
-        } else {
-            listItem.classList.remove("active");
+const callback = entries => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0.8) {
+            colors.forEach(color => {
+                color.classList.add(animateClass)
+            })
         }
-    }
+        else if (entry.intersectionRatio < 0.79 && entry.boundingClientRect.y > -200) {
+            colors.forEach(color => {
+                color.classList.remove(animateClass)
+            })
+        }
+    })
 }
 
-function isPartiallyVisible(el) {
-    let elementBoundary = el.getBoundingClientRect();
+let observer = new IntersectionObserver(callback, options)
 
-    let top = elementBoundary.top;
-    let bottom = elementBoundary.bottom;
-    let height = elementBoundary.height;
+observer.observe(section)
 
-    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
-}
+// carousel
 
-function isFullyVisible(el) {
-    let elementBoundary = el.getBoundingClientRect();
-
-    let top = elementBoundary.top;
-    let bottom = elementBoundary.bottom;
-
-    return ((top >= 0) && (bottom <= window.innerHeight));
-}

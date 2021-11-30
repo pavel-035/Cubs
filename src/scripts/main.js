2d6395dabf4a855412dd3760876a0b5edfeb5649
pavel-035 <kdpavel8@gmail.com>
2021-11-30 15:39:29 +0500

    asd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,6 @@ window.addEventListener('mousemove', function (e) {
     let windowWidth = window.innerWidth / 2;
     let windowHeight = window.innerHeight / 2;
 
-    console.log(windowWidth);
     if(windowWidth >= 960) {
         translateBlock.forEach(function(item) {
             let translateSpeed = item.getAttribute('data-animateSpeed')
